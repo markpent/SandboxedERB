9e5f0ecce9588ef006214315f785684b84e81c4a
MarkPent <mark.pent@gmail.com>
2011-06-04 20:05:30 +1000

    add licence

--- lib/sandboxed_erb/template.rb
+++ lib/sandboxed_erb/template.rb
@@ -1,3 +1,24 @@
+=begin
+
+This file is part of the sandboxed_erb project, https://github.com/markpent/SandboxedERB
+
+Copyright (c) 2011 Mark Pentland <mark.pent@gmail.com>
+
+sandboxed_erb is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+sandboxed_erb is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with shikashi.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+
 require "erb"
 require "partialruby"
 
